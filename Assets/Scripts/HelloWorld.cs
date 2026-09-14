8eea663308c0e81d5170ebfbcdc5ba91b631e4a6
PClark465 <pclark85738@uat.edu>
2026-09-13 19:50:05 -0700

    Initialized Repo

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //This line of code will be used to output to the console
+        Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
